c75520a30b3c50990252a360022f8c90494f78bd
altamash <altamkhan@ebay.com>
2026-02-07 12:00:08 +0530

    remove unused badge and label components along with related utility functions and tests

--- src/lib/utils/init_theme.ts
+++
@@ -1,6 +0,0 @@
-import type { ThemePort } from '$lib/ports/theme_port'
-import type { ThemeMode } from '$lib/types/theme'
-
-export function init_theme(theme_port: ThemePort): ThemeMode {
-  return theme_port.get_theme()
-}
